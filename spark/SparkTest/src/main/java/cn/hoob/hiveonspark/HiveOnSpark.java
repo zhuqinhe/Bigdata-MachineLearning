f07e85d7b1b9db8a8a922b4682c645ada8972450
hoobzhu <qinhezhu@yeah.net>
2020-04-22 22:23:20 +0800

    hive on spark

---
+++ spark/SparkTest/src/main/java/cn/hoob/hiveonspark/HiveOnSpark.java
@@ -0,0 +1,27 @@
+package cn.hoob.hiveonspark;
+
+import cn.hoob.aceessprocess.IpRules;
+import cn.hoob.aceessprocess.IpUtils;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoder;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.SparkSession;
+
+/**
+ * spark SQL 分析访问日子
+ */
+public class HiveOnSpark {
+    public static void main(String[] args) {
+
+        System.setProperty("HADOOP_USER_NAME", "root");
+        SparkSession sparkSession= SparkSession.builder().appName("HiveOnSpark").master("local[*]").enableHiveSupport().getOrCreate();
+        //Dataset  hivedataSet= sparkSession.sql("select * from t_access");
+        //hivedataSet.show();
+        sparkSession.sql("drop table hoob_t_a");
+        sparkSession.sql("create table hoob_t_a(name string,numb int) row format delimited fields terminated by ','");
+        sparkSession.sql("load data   inpath '/a.txt' into table hoob_t_a ");
+        sparkSession.sql("select * from hoob_t_a").show();
+        sparkSession.stop();
+    }
+}
