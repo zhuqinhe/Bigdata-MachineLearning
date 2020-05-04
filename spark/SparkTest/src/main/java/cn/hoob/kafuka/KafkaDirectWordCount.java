22aa4cadbcf1a2d52650ee28b850c8f925dd005c
hoobzhu <qinhezhu@yeah.net>
2020-05-04 15:59:13 +0800

    读取kafka数据

--- spark/SparkTest/src/main/java/cn/hoob/kafuka/KafkaDirectWordCount.java
+++ spark/SparkTest/src/main/java/cn/hoob/kafuka/KafkaDirectWordCount.java
@@ -8,6 +8,7 @@ import kafka.utils.ZkUtils;
 import org.I0Itec.zkclient.ZkClient;
 import org.apache.commons.codec.StringDecoder;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
@@ -52,13 +53,24 @@ public class KafkaDirectWordCount {
         kafkaParams.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         kafkaParams.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         kafkaParams.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        kafkaParams.put("auto.offset.reset", "latest");
+        // latest  当各分区下有已提交的offset时，从提交的offset开始消费；无提交的offset时，消费新产生的该分区下的数据
+        // earliest  当各分区下有已提交的offset时，从提交的offset开始消费；无提交的offset时，从头开始消费
+        // none  当各分区下都存在已提交的offset时，从offset后开始消费；只要有一个分区不存在已提交的offset，则抛出异常
+        kafkaParams.put("auto.offset.reset", "earliest");//
+        //true 偶然false  控制kafka是否自动提交偏移量
+        kafkaParams.put("enable.auto.commit",true);//
+
 
         //创建Kafka的topics ，里面可以填多个topic
         Collection<String> topics=Arrays.asList("hoobtest");
-
+        //Topic分区
+        //Map<TopicPartition, Long> offsets = new HashMap<>();
+        //offsets.put(new TopicPartition("topic1", 0), 2L);
+      //  JavaInputDStream<ConsumerRecord<Object, Object>> lines = KafkaUtils.createDirectStream(jsc, LocationStrategies.PreferConsistent(),
+        //        ConsumerStrategies.Subscribe(topics, kafkaParams,offsets));
         //创建DStream
-        JavaInputDStream<ConsumerRecord<Object, Object>> lines = KafkaUtils.createDirectStream(jsc, LocationStrategies.PreferConsistent(), ConsumerStrategies.Subscribe(topics, kafkaParams));
+        JavaInputDStream<ConsumerRecord<Object, Object>> lines = KafkaUtils.createDirectStream(jsc, LocationStrategies.PreferConsistent(),
+                ConsumerStrategies.Subscribe(topics, kafkaParams));
 
         //拆分Kafka topic里面的数据
         JavaDStream<String> linesSplit=lines.flatMap(new FlatMapFunction<ConsumerRecord<Object, Object>, String>() {
@@ -83,7 +95,6 @@ public class KafkaDirectWordCount {
             }
         });
         wordsCount.print();
-
         jsc.start();
         jsc.awaitTermination();
         jsc.close();
