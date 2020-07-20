e2d7f213393da488b0d3a6d7d331f9d88bc3e855
hoobzhu <qinhezhu@yeah.net>
2020-07-20 17:40:31 +0800

    Summery statistic(汇总统计)

---
+++ spark/SparkTest/src/main/java/cn/hoob/recommenddemo/BasicStistics/SummaryStatistics.java
@@ -0,0 +1,66 @@
+package cn.hoob.recommenddemo.BasicStistics;
+
+import cn.hoob.recommenddemo.similarity.SimilarityDataModel;
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.ml.stat.Correlation;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.Vectors;
+import org.apache.spark.mllib.linalg.distributed.CoordinateMatrix;
+import org.apache.spark.mllib.linalg.distributed.MatrixEntry;
+import org.apache.spark.mllib.stat.MultivariateStatisticalSummary;
+import org.apache.spark.mllib.stat.Statistics;
+import org.apache.spark.rdd.RDD;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import scala.collection.Seq;
+
+/***
+ *  Basic Statistics
+ *  分为：
+ *  1、Summery statistic(汇总统计)
+ *
+ * 2、Correlations(相关系数)
+ *
+ * 3、Stratified sampling（分层抽样）
+ *
+ * 4、Hypothesis Testing(假设检验)
+ *
+ * 5、Random data generation(随机数生成)
+ *
+ * 6、Kernel density estimation（核密度估计）
+ * 这里的是汇总统计
+ **/
+public class SummaryStatistics {
+    public static void main(String[] args) throws IOException {
+        System.setProperty("HADOOP_USER_NAME", "root");
+        SparkSession sparkSession = SparkSession.builder().appName("SimilarityModelApp").
+                master("local[*]")
+                .config("spark.sql.shuffle.partitions", "2").enableHiveSupport().getOrCreate();
+        //sparksession to javaSparkContext
+        SparkContext sc=sparkSession.sparkContext();
+        JavaSparkContext jsc = new JavaSparkContext(sc);
+
+        List<Vector>vectors=new ArrayList<>();
+        //稠密向量
+        vectors.add(Vectors.dense(4.0, 5.0, 0.0, 3.0));
+        vectors.add(Vectors.dense(6.0, 7.0, 0.0, 8.0));
+        //稀疏向量
+        vectors.add(Vectors.sparse(4, new int[] {0, 1}, new double[] {1.0,-2.0}));
+        vectors.add(Vectors.sparse(4, new int[] {0, 3}, new double[] {9.0, 1.0}));
+        JavaRDD<Vector> mat=jsc.parallelize(vectors);
+        MultivariateStatisticalSummary summary = Statistics.colStats(mat.rdd());
+        System.out.println(summary.mean());
+        System.out.println(summary.variance());
+        System.out.println(summary.numNonzeros());
+    }
+}
