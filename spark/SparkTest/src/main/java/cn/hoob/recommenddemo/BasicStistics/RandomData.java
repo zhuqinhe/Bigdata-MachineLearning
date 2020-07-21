76fd61e33e26171abc7dad911461092ce5e49272
hoobzhu <qinhezhu@yeah.net>
2020-07-21 17:35:13 +0800

    随机数生成

---
+++ spark/SparkTest/src/main/java/cn/hoob/recommenddemo/BasicStistics/RandomData.java
@@ -0,0 +1,50 @@
+package cn.hoob.recommenddemo.BasicStistics;
+
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaDoubleRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.linalg.Matrices;
+import org.apache.spark.mllib.linalg.Matrix;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.Vectors;
+import org.apache.spark.mllib.random.RandomRDDs;
+import org.apache.spark.mllib.regression.LabeledPoint;
+import org.apache.spark.mllib.stat.Statistics;
+import org.apache.spark.mllib.stat.test.ChiSqTestResult;
+import org.apache.spark.sql.SparkSession;
+
+import java.io.IOException;
+import java.util.Arrays;
+/***
+ *  Basic Statistics
+ *  分为：
+ *  1、Summery statistic(汇总统计)
+ *
+ * 2、Correlations(相关系数)
+ *
+ * 3、Stratified sampling（分层抽样）
+ *
+ * 4、Hypothesis Testing(假设检验)
+ *
+ * 5、Random data generation(随机数生成)
+ *
+ * 6、Kernel density estimation（核密度估计）
+ *
+ *    这里的是随机数生成
+ *
+ **/
+public class RandomData {
+    public static void main(String[] args) throws IOException {
+        System.setProperty("HADOOP_USER_NAME", "root");
+        SparkSession sparkSession = SparkSession.builder().appName("CorrelationsStatistics").
+                master("local[*]")
+                .config("spark.sql.shuffle.partitions", "2").enableHiveSupport().getOrCreate();
+        //sparksession to javaSparkContext
+        SparkContext sc=sparkSession.sparkContext();
+        JavaSparkContext jsc = new JavaSparkContext(sc);
+        JavaDoubleRDD u = RandomRDDs.normalJavaRDD(jsc, 1000000L, 10);
+        JavaDoubleRDD v = u.mapToDouble(x -> 1.0 + 2.0 * x);
+        System.out.println(v.collect());
+    }
+}
